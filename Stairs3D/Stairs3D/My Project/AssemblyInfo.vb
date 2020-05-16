a2e6beb053e59658cbcb4a7af12c659e1bb6476b
Kamil Martenczuk <kamil.martenczuk@cadcenterpl.onmicrosoft.com>
2020-05-16 23:24:54 +0200

    v1.0

---
+++ Stairs3D/Stairs3D/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Ogólne informacje o zestawie są kontrolowane poprzez następujący 
+' zestaw atrybutów. Zmień wartości tych atrybutów, aby zmodyfikować informacje
+' powiązane z zestawem.
+
+' Sprawdź wartości atrybutów zestawu
+
+<Assembly: AssemblyTitle("Stairs3D")>
+<Assembly: AssemblyDescription("Software for Autodesk AutoCAD")>
+<Assembly: AssemblyCompany("Kamil Martenczuk DEPL2710")>
+<Assembly: AssemblyProduct("Stairs3D")>
+<Assembly: AssemblyCopyright("Copyright ©  2020 Kamil Martenczuk")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(True)>
+
+'Następujący identyfikator GUID jest identyfikatorem elementu typelib w przypadku udostępnienia tego projektu w modelu COM
+<Assembly: Guid("b9ba6434-421b-466a-9b5c-d9bf0707ac49")>
+
+' Informacje o wersji zestawu zawierają następujące cztery wartości:
+'
+'      Wersja główna
+'      Wersja pomocnicza
+'      Numer kompilacji
+'      Poprawka
+'
+' Możesz określić wszystkie wartości lub użyć domyślnych numerów kompilacji i poprawki
+' przy użyciu symbolu „*”, tak jak pokazano poniżej:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
